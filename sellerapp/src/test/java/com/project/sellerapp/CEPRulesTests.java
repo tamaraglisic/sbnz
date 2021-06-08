66158237ea0a0630400371a5cacdd7fc1a7cb72a
tamaraglisic <39412716+tamaraglisic@users.noreply.github.com>
2021-06-08 20:02:29 +0200

    cep for many reservation requests

--- sellerapp/src/test/java/com/project/sellerapp/CEPRulesTests.java
+++ sellerapp/src/test/java/com/project/sellerapp/CEPRulesTests.java
@@ -1,24 +1,26 @@
 package com.project.sellerapp;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
-import static org.junit.Assert.assertEquals;
-
 
 import com.project.sellerapp.dto.LoginEvent;
+import com.project.sellerapp.dto.ReservationEvent;
 
 public class CEPRulesTests {
 	
 
 
 	@Test
-    public void testCEPRules() {
+    public void testLoginCEPRules() {
 		KieServices ks = KieServices.Factory.get();
 	    KieContainer kContainer = ks.newKieContainer(ks.newReleaseId("com.main", "sellerapp-drools", "0.0.1-SNAPSHOT"));
 
 	    KieSession session = kContainer.newKieSession("cepSession");
+	  //  session.getAgenda().getAgendaGroup("login").setFocus();
 
 	    session.insert(new LoginEvent("admin@gmail.com"));
 	    session.insert(new LoginEvent("admin@gmail.com"));
@@ -31,4 +33,29 @@ public class CEPRulesTests {
 
 	    assertEquals(1, firedRules);
     }
+	
+	@Test
+    public void testReservationCEPRules() {
+		KieServices ks = KieServices.Factory.get();
+	    KieContainer kContainer = ks.newKieContainer(ks.newReleaseId("com.main", "sellerapp-drools", "0.0.1-SNAPSHOT"));
+
+	    KieSession session = kContainer.newKieSession("cepSession");
+	  //  session.getAgenda().getAgendaGroup("reservations").setFocus();
+	    
+	    session.insert(new ReservationEvent("admin@gmail.com"));
+	    session.insert(new ReservationEvent("admin@gmail.com"));
+	    session.insert(new ReservationEvent("admin@gmail.com"));
+	    session.insert(new ReservationEvent("admin@gmail.com"));
+	    session.insert(new ReservationEvent("admin@gmail.com"));
+	    session.insert(new ReservationEvent("admin@gmail.com"));
+	    session.insert(new ReservationEvent("admin@gmail.com"));
+	    session.insert(new ReservationEvent("admin@gmail.com"));
+
+	    int firedRules = session.fireAllRules();
+        session.dispose();
+
+	    assertEquals(1, firedRules);
+    }
+	
+	
 }
